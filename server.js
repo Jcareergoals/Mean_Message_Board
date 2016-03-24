1d36e6ef6752a1ef05844b9461f251cddb717cdf
Jose Chery <Jcareergoals@hotmail.com>
2016-03-24 01:52:26 -0400

    added comment feature where users can comment on individual messages. Comments for each message are also displayed in descending order.

--- server.js
+++ server.js
@@ -13,17 +13,35 @@ app.set('view engine', 'ejs');
 
 mongoose.connect('mongodb://localhost/message_board'); 
 
+var Schema = mongoose.Schema; 
+
+// message model schema 
 var messageSchema = new mongoose.Schema({
 	name: String,
 	text: String, 
+	comments: [{type: Schema.Types.ObjectId, ref: 'comments'}], 
 	date: {type: Date, default: new Date}
 }); 
-mongoose.model('messages', messageSchema); 
-var messages = mongoose.model('messages');
+var commentSchema = new mongoose.Schema({
+	_messages: {type: Schema.Types.ObjectId, ref: 'messages'},
+	name: String, 
+	text: String, 
+	date: {type: Date, default: new Date}
+});
+mongoose.model('messages', messageSchema);
+mongoose.model('comments', commentSchema); 
 
+var messages = mongoose.model('messages');
+var comments = mongoose.model('comments');
 // ROUTES
 app.get('/', function(req, res){
-	messages.find({}, function(err, data){
+	// messages.find({}, function(err, data){
+	// 	res.render('index', {messages:data}); 
+	// });
+	// ===== Use the commented out code above to exclude comments ==== 
+	messages.find({})
+	.populate('comments') 
+	.exec(function(err, data){
 		res.render('index', {messages:data}); 
 	});
 });
@@ -41,13 +59,53 @@ app.post('/messages', function(req, res){
   		}
 	}); 
 });
+
+
+// To send comment data to db 
 app.post('/comments/:id', function(req, res){
-	console.log("COMMENT: "); 
-	console.log("_id:"+req.params.id); 
-	console.log(req.body);
-	console.log("=================="); 
-	res.redirect('/'); 
+	messages.findOne({_id: req.params.id}, function(err, message){
+		var commentData = new comments({
+			name: req.body.name, 
+			text: req.body.text, 
+			date: new Date
+		});
+		console.log("This is comment data");
+		console.log(commentData);
+		console.log("This is the message data"); 
+		console.log(message);
+		// link the two collections 
+		commentData._messages = message._id;
+		// push comments to comment array in messages collection 
+		message.comments.push(commentData);
+		message.comments.push(commentData._msg);
+		// save comment to data
+		console.log('data was pushed');
+		commentData.save(function(err){
+			message.save(function(err){
+				if(err){
+					console.log('Error!:');
+					console.log(err); 
+				} else {
+					res.redirect('/');
+				}
+			});
+		}); 
+	});
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(8888, function(){
 	console.log("Listening at port: 8888"); 
